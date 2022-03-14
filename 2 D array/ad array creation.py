4eb2971222f9095179ee42c6fb5be7449677c639
VidushiShekhar <shekharvidushi@gmail.com>
2022-03-14 23:53:02 +0530

    addition

--- 2 D array/ad array creation.py
+++ 2 D array/ad array creation.py
@@ -44,3 +44,28 @@ arr[0][0] = 1
 for row in arr:
     print(row)
 # both the technique have a different method the object are assigned differently
+# better way to declare 2D array:
+rows, cols = (3, 5)
+arr = [[0 for i in range(cols)] for j in range(rows)]
+"""Hence the better way to declare a 2d array is 
+"""
+rows, cols = (5, 5)
+arr = [[0 for i in range(cols)] for j in range(rows)]
+# the above method create separate list
+""" using is operator to check for the same object"""
+
+arr =[[0 for i in range(cols)] for j in range(rows)]
+# check if arr[0] and arr[1] refer to same object
+print(arr[0] is arr[1])
+arr[1][0] = 1
+print(arr[0][0] is arr[1][0])
+
+# method 2a
+
+arr = [[0]*cols]*rows
+
+print(arr[0] is arr[1])
+# the above function print True
+
+
+
