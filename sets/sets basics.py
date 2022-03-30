a58bb2dab5dce53ea3b671df29664097e42c4f2d
VidushiShekhar <shekharvidushi@gmail.com>
2022-03-30 23:47:30 +0530

    addition

--- sets/sets basics.py
+++ sets/sets basics.py
@@ -1,3 +1,20 @@
 """A set is an unordered collection data type that is iterable,
 mutable and has no duplicate elements"""
 
+# python program to demonstrate sets
+# Same as {"a", "b", "c"}
+myset = set(["a", "b", "c"])
+print(myset)
+
+myset.add('d')
+print(myset)
+
+"""Frozen Sets in python are immutable objects """
+
+
+normal_set = set(["a", "b", " c"])
+print(normal_set)
+# A frozen Set
+frozen_set = frozenset(["e", "f", "g"])
+print()
+
