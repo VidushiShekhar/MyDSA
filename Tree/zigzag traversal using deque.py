ce110bda9ffb504cbf7d9ebc213a6def38033ffc
VidushiShekhar <shekharvidushi@gmail.com>
2022-04-07 10:39:08 +0530

    addition

--- Tree/zigzag traversal using deque.py
+++ Tree/zigzag traversal using deque.py
@@ -1,23 +1,32 @@
+# Python3 implementation of a O(n) time method for
+# Zigzag order traversal
 from collections import deque
 
+
+# Binary Tree node
 class Node:
     def __init__(self, data):
         self.data = data
         self.left = None
         self.right = None
 
-def ZigZagTraversal(root):
+
+# Function to print the zigzag traversal
+def zigZagTraversal(root):
     q = deque([])
     v = []
     q.append(root)
     v.append(root.data)
 
+    # set initial level as 1, because root is
+    # already been taken care of.
     l = 1
 
     while len(q) > 0:
         n = len(q)
         for i in range(n):
-            if l % 2 == 0:
+            # popping mechanism
+            if (l % 2 == 0):
                 temp = q[-1]
                 q.pop()
             else:
@@ -29,16 +38,22 @@ def ZigZagTraversal(root):
                 if temp.right:
                     q.append(temp.right)
                     v.append(temp.right.data)
+                if temp.left:
+                    q.append(temp.left)
+                    v.append(temp.left.data)
             elif l % 2 == 0:
                 if temp.left:
                     q.appendleft(temp.left)
                     v.append(temp.left.data)
-                if temp.right:
+                if (temp.right):
                     q.appendleft(temp.right)
                     v.append(temp.right.data)
+        l += 1  # level plus one
+    return v
 
-            l += 1 # level plus one
-        return v
+
+# vector to store the traversal order.
+v = []
 
 # create tree
 root = Node(1)
@@ -48,12 +63,9 @@ root.left.left = Node(7)
 root.left.right = Node(6)
 root.right.left = Node(5)
 root.right.right = Node(4)
-print("zigzag order traversal of binary tree")
-v = ZigZagTraversal(root)
+print("ZigZag Order traversal of binary tree is")
+
+v = zigZagTraversal(root)
 
 for i in range(len(v)):
-    print(v[i], end=' ')
-
-
-
-
+    print(v[i], end=" ")
