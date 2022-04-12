f293cd6d9f9b5128bf2fb2ef162e339061264c68
VidushiShekhar <shekharvidushi@gmail.com>
2022-04-12 22:22:19 +0530

    addition

--- Tree/Tree from Inorder and level order traversal.py
+++ Tree/Tree from Inorder and level order traversal.py
@@ -1,58 +1,47 @@
-# construction of Tree using level order traversal
-
-# A binary tree node
-
 class Node:
+
     def __init__(self, key):
         self.data = key
         self.left = None
         self.right = None
 
 
-
 def buildTree(level, ino):
 
+    # If ino array is not empty
     if ino:
-
+        # Check if that element exist in level order
         for i in range(0, len(level)):
+
             if level[i] in ino:
                 node = Node(level[i])
+
                 io_index = ino.index(level[i])
                 break
+    # Construction of the left and right subtree
 
-
-        # Construct left and right subtree
-        node.left = buildTree(level, ino[0: io_index])
+        node.left = buildTree(level, ino[0:io_index])
         node.right = buildTree(level, ino[io_index+1: len(ino)])
         return node
     else:
         return None
 
-
 def printInorder(node):
     if node is None:
         return
-    printInorder(node.left)
-    print(node.data, end=" ")
 
+    printInorder(node.left)
+
+    print(node.data, end=" ")
     printInorder(node.right)
 
 
-# Driver code
-
+# Driver Code
 levelorder = [20, 8, 22, 4, 12, 10, 14]
-inorder =  [4, 8, 12, 14, 20, 22]
+inorder = [4, 8, 10, 12, 14, 20, 22]
 
 ino_len = len(inorder)
 root = buildTree(levelorder, inorder)
 
 print("Inorder traversal of the constructed tree is")
 printInorder(root)
-
-
-
-
-
-
-
-
